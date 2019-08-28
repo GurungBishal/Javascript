c0e0ea3941bb284980493554608fc5d09e301774
gurungitachi <gurung.biishal88@gmail.com>
2019-08-28 20:51:25 +0545

    'agdg'

---
+++ Github Finder/github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = '09e3459053eaee2e693f';
+        this.client_secret = '801c7c61ce4b0bff23cdce742a9683f5b51994ad';
+        this.repos_count = 5;
+        this.repos_sort = 'created: async';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user} ? client_id =${this.client_id} & client_secret = ${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos? per_page = ${this.repos_count} & sort = ${this.repos_sort} & client_id =${this.client_id} & client_secret = ${this.client_secret}`);
+
+        const profileData = await profileResponse.json();
+        const repoData = await repoResponse.json();
+
+        return {
+            profile: profileData,
+            repo: repoData
+        }
+    }
+}
